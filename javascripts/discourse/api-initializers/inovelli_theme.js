6d1ed58f82ae3c0973bfb3306cf157232fd6c72d
Eric Maycock <ericm@inovelli.com>
2025-05-30 23:48:34 -0600

    Additional updates to improve compatibility

--- javascripts/discourse/api-initializers/inovelli_theme.js
+++ javascripts/discourse/api-initializers/inovelli_theme.js
@@ -1,11 +1,25 @@
 import getURL from 'discourse-common/lib/get-url';
 import { apiInitializer } from 'discourse/lib/api';
 import { h } from 'virtual-dom';
-import { schedule } from '@ember/runloop';
-import Component from '@ember/component';
 
-export default apiInitializer('1.8.0', (api) => {
-  // Register custom menu panel for below-site-header
+export default apiInitializer('0.11.1', (api) => {
+  // Fix prefers dark theme and toggle issues:
+  api.reopenWidget('home-logo', {
+    logoElement(key, url, title) {
+      const attributes =
+        key === 'logo-small'
+          ? { src: getURL(url), width: 36, alt: title }
+          : { src: getURL(url), alt: title };
+
+      const imgElement = h(`img#site-logo.${key}`, {
+        key,
+        attributes,
+      });
+
+      return imgElement;
+    },
+  });
+
   api.registerConnectorClass('below-site-header', 'inovelli-menu-panel', {
     setupComponent(attrs, component) {
       const menuData = settings.menu_items.split('|');
@@ -14,23 +28,40 @@ export default apiInitializer('1.8.0', (api) => {
       const subMenuItems = [];
 
       subMenuData.forEach((item) => {
-        const [parent, title, url] = item.split(',');
-        subMenuItems.push({ parent, title, url });
+        const subMenuItem = item.split(',');
+        const subMenuObject = {
+          parent: subMenuItem[0],
+          title: subMenuItem[1],
+          url: subMenuItem[2],
+        };
+
+        subMenuItems.push(subMenuObject);
       });
 
       menuData.forEach((item) => {
-        const [title, url, hasChildren] = item.split(',');
-        const menuObject = { title, url, hasChildren: !!hasChildren };
+        const menuItem = item.split(',');
+
+        const menuObject = {
+          title: menuItem[0],
+          url: menuItem[1],
+          hasChildren: menuItem[2],
+        };
+
+        // if item has sub-menu items, find items and append to object:
         if (menuObject.hasChildren) {
-          menuObject.children = subMenuItems.filter(
-            (subItem) => subItem.parent === menuObject.title
-          );
+          menuObject.children = [];
+          subMenuItems.map((subItem) => {
+            if (subItem.parent === menuObject.title) {
+              menuObject.children.push(subItem);
+            }
+          });
         }
         menuItems.push(menuObject);
       });
 
       component.set('menuItems', menuItems);
     },
+
     actions: {
       returnMain() {
         this.set('showSubMenu', '');
@@ -38,26 +69,46 @@ export default apiInitializer('1.8.0', (api) => {
     },
   });
 
-  // Register the menu button component
-  api.registerComponent('inovelli-menu-button', {
-    templateName: 'components/inovelli-menu-button',
-    actions: {
-      toggleMenu() {
-        const bodyClass = 'inovelli-menu-active';
-        schedule('afterRender', () => {
-          if (document.body.classList.contains(bodyClass)) {
-            document.body.classList.remove(bodyClass);
-          } else {
-            document.body.classList.add(bodyClass);
-          }
-        });
-      }
-    }
+  api.decorateWidget('header-contents:before', (helper) => {
+    return helper.attach('inovelli-menu');
   });
 
-  // Add menu button to header using the new API
-  api.headerButtons.add("inovelli-menu", {
-    template: "components/inovelli-menu-button",
-    before: "auth"
+  api.createWidget('inovelli-menu', {
+    tagName: 'nav.inovelli-menu',
+    buildKey: (attrs) => `inovelli-menu-button-${attrs.id}`,
+
+    defaultState() {
+      return {
+        active: 'inactive',
+      };
+    },
+
+    html(attrs, state) {
+      const hamburgerButton = [
+        h('span.sr-only', 'Menu'),
+        h('span.bar-top', ''),
+        h('span.bar-middle', ''),
+        h('span.bar-bottom', ''),
+      ];
+
+      const menuButton = h(
+        `div.inovelli-menu-toggle.${state.active}`,
+        hamburgerButton
+      );
+
+      return menuButton;
+    },
+
+    click() {
+      if (this.state.active === 'inactive') {
+        document.body.classList.add('inovelli-menu-active');
+        this.state.active = 'active';
+      } else {
+        document.body.classList.remove('inovelli-menu-active');
+        this.state.active = 'inactive';
+      }
+    },
   });
+
+  api.replaceIcon('bars', 'cog');
 });
